fc57ace6765d13de3446bf4835e3db791b74e9e8
Lev Gadasin <lev.gadasin@gmail.com>
2026-05-20 00:19:19 +0300

    1. Improve code formatting according to standards 2. Add final and constans for max and min rating 3. Remove the director condition from checking if the movie exists 4. Added successful movie addition message even after updating

--- hw1/src/Main.java
+++ hw1/src/Main.java
@@ -6,11 +6,14 @@ import java.util.Scanner;
 
 public class Main {
     public static Scanner scanner; // Note: Do not change this line.
-    public static int MAX_SIZE = 100;
+    public static final int MAX_SIZE = 100;
+    public static final double MIN_RATING = 0;
+    public static final double MAX_RATING = 10;
     public static String[] movies;
     public static String[] directors;
     public static double[] ratings;
     public static int numberOfMovies = 0;
+
     public static void manageMovies() {
         // core movie management function
         String choice;
@@ -24,29 +27,36 @@ public class Main {
         // main routing loop
         while (true) {
             System.out.println("""
-                1. Add a new movie
-                2. Display all movies
-                3. Display movie rating
-                4. Find the best director
-                5. Exit""");
+                    1. Add a new movie
+                    2. Display all movies
+                    3. Display movie rating
+                    4. Find the best director
+                    5. Exit""");
             System.out.println("Please enter your choice:");
             choice = scanner.nextLine();
-            switch (choice){
+            switch (choice) {
                 case "1":
-                    addMovie(scanner); break;
+                    addMovie(scanner);
+                    break;
                 case "2":
-                    displayAll(); break;
+                    displayAll();
+                    break;
                 case "3":
-                    displayRating(); break;
+                    displayRating();
+                    break;
                 case "4":
-                    findBest(); break;
+                    findBest();
+                    break;
                 case "5":
-                    exit(); return;
+                    exit();
+                    return;
                 default:
-                    System.out.println("Invalid choice. Please try again."); break;
+                    System.out.println("Invalid choice. Please try again.");
+                    break;
             }
         }
     }
+
     public static void addMovie(Scanner scanner) {
         // adds a movie to the first empty array index
         System.out.println("Enter movie name:");
@@ -55,7 +65,7 @@ public class Main {
         System.out.println("Enter rating:");
         double rating = scanner.nextDouble();
         scanner.nextLine();
-        if(rating < 0 || rating > 10) {
+        if (rating < MIN_RATING || rating > MAX_RATING) {
             System.out.println("Invalid rating");
             return;
         }
@@ -63,66 +73,69 @@ public class Main {
         String directorName = scanner.nextLine();
 
         // check if the movie already exists or if the limit has been reached
-        if(!movieExist(movieName,directorName)) {
+        if (!movieExist(movieName)) {
             if (numberOfMovies < MAX_SIZE) {
                 movies[numberOfMovies] = movieName;
                 directors[numberOfMovies] = directorName;
                 ratings[numberOfMovies] = rating;
                 numberOfMovies++;
-                System.out.println("Movie " + movieName + " added successfully!");
-            }
-            else {
+            } else {
                 System.out.println("Movies limit reached");
                 return;
             }
         } else {
-            updateMovie(movieName,rating);
+            updateMovie(movieName, rating);
         }
+        System.out.println("Movie " + movieName + " added successfully!");
     }
-    public static boolean movieExist(String movieName,String directorName){
+
+    public static boolean movieExist(String movieName) {
         // returns true if the movie with the corresponding director exists
-       for (int i = 0; i < numberOfMovies; i++) {
-           if(movies[i].equals(movieName) && directors[i].equals(directorName)) return true;
-       }
-       return false;
+        for (int i = 0; i < numberOfMovies; i++) {
+            if (movies[i].equals(movieName)) return true;
+        }
+        return false;
     }
-    public static void updateMovie(String movieName,double rating) {
+
+    public static void updateMovie(String movieName, double rating) {
         // updates the existing movie with the new rating
         for (int i = 0; i < numberOfMovies; i++) {
-            if(movies[i].equals(movieName)) {
+            if (movies[i].equals(movieName)) {
                 ratings[i] = rating;
             }
         }
 
     }
+
     public static void displayAll() {
         // displays all the existing movies with theirs directors and ratings
         if (numberOfMovies == 0) {
             System.out.println("No movies are available");
             return;
         }
-        if(directors == null) return;
+        if (directors == null) return;
         // sort the directors in lexicographical ascending order
         String[] uniqueSortedDirectors = Arrays.stream(directors)
                 .filter(Objects::nonNull)
                 .distinct()
                 .toArray(String[]::new);
         // print movies and rating of each director
-        for(String author: uniqueSortedDirectors) {
+        for (String author : uniqueSortedDirectors) {
             for (int i = 0; i < numberOfMovies; i++) {
-                if(!directors[i].equals(author)) continue;
+                if (!directors[i].equals(author)) continue;
                 System.out.println("Name: " + movies[i] + " rating: " + ratings[i] +
                         " director: " + directors[i]);
             }
         }
     }
+
     public static void displayRating() {
         // displays the rating of the entered movie
         System.out.println("Enter movie name:");
         String movieName = scanner.nextLine();
 
-        for (int i = 0; i < numberOfMovies; i++){
-            if (movieName.equals(movies[i])){
+        for (int i = 0; i < numberOfMovies; i++) {
+            if (movieName.equals(movies[i])) {
                 System.out.printf("Rating for %s: %.1f%n", movieName, ratings[i]);
                 return;
             }
@@ -130,9 +143,10 @@ public class Main {
         System.out.printf("No movie found with name %s%n", movieName);
 
     }
+
     public static void findBest() {
         // find the director with the best average rating
-        if (numberOfMovies == 0){
+        if (numberOfMovies == 0) {
             System.out.println("No movies are available.");
             return;
         }
@@ -149,11 +163,11 @@ public class Main {
 
         // go over each director until the end, increasing the sum and count
         // make the "visited" directors empty, to skip unnecessary loops
-        for (int i = 0; i < numberOfMovies; i++){
+        for (int i = 0; i < numberOfMovies; i++) {
             if (copyDirectors[i].isEmpty()) continue;
             currAuthor = copyDirectors[i];
-            for (int j = i; j < numberOfMovies; j++){
-                if (currAuthor.equals(copyDirectors[j])){
+            for (int j = i; j < numberOfMovies; j++) {
+                if (currAuthor.equals(copyDirectors[j])) {
                     countMovies++;
                     sumRatings += ratings[j];
                     copyDirectors[j] = "";
@@ -161,7 +175,7 @@ public class Main {
             }
             // find the best average rating
             avg = sumRatings / countMovies;
-            if (avg > bestRating){
+            if (avg > bestRating) {
                 bestRating = avg;
                 bestAuthor = currAuthor;
             }
@@ -173,10 +187,11 @@ public class Main {
     }
 
 
-    public static void exit(){
+    public static void exit() {
         // prints the exit message
         System.out.println("Exiting the program. Goodbye!");
     }
+
     public static void main(String[] args) throws IOException {
         String path = args[0];
 
@@ -188,7 +203,7 @@ public class Main {
             System.out.println("Test number " + i + " starts.");
             try {
                 manageMovies();
-            } catch(Exception e){
+            } catch (Exception e) {
                 System.out.println("Exception " + e);
             }
             System.out.println("Test number " + i + " ended.");

304290edab0c5105d48972986be12fda7e6873f7
Lev Gadasin <lev.gadasin@gmail.com>
2026-05-19 17:22:30 +0300

    Setup main function and core logic.

--- hw1/src/Main.java
+++ hw1/src/Main.java
@@ -4,17 +4,38 @@ import java.util.Scanner;
 
 public class Main {
     public static Scanner scanner; // Note: Do not change this line.
-// add comments here!! + refactor names !!
+    public static String[] movies = new String[100];
+    public static String[] authors = new String[100];
+    public static double[] ratings = new double[100];
     public static void manageMovies() {
+        String choice;
 
+        System.out.println("Welcome to the Movies Management System!.");
+        // add comments here!! + refactor names !!
+        System.out.println("""
+                1. Add a new movie
+                2. Display all movies
+                3. Display movie rating
+                4. Find the best director
+                5. Exit""");
 
-
-
-
-
-
+        while (true) {
+            System.out.println("Please enter your choice:");
+            choice = scanner.next();
+            switch (choice){
+                case "1":
+                    addMovie(); break;
+                case "2":
+                    displayAll(); break;
+                case "3":
+                    displayRating(); break;
+                case "4":
+                    findBest(); break;
+                case "5":
+                    exit(); break;
+            }
+        }
     }
-
     public static void addMovie() {}
     public static void displayAll() {}
     public static void displayRating() {}
@@ -23,6 +44,7 @@ public class Main {
 
     public static void exit(){
         System.out.println("Exiting the program. Goodbye!");
+        System.exit(0);
     }
     public static void main(String[] args) throws IOException {
         String path = args[0];

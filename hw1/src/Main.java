f55d946c69e93e35f96cd50c57b16c8763dcab8c
Lev Gadasin <lev.gadasin@gmail.com>
2026-05-19 16:45:08 +0300

    Homework one attached files

--- hw1/src/Main.java
+++ hw1/src/Main.java
@@ -1,13 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+public class Main {
+    public static Scanner scanner; // Note: Do not change this line.
+
+    public static void manageMovies() {}
+
+    public static void main(String[] args) throws IOException {
+        String path = args[0];
+
+        scanner = new Scanner(new File(path));
+        int numberOfTests = scanner.nextInt();
+        scanner.nextLine();
+
+        for (int i = 1; i <= numberOfTests; i++) {
+            System.out.println("Test number " + i + " starts.");
+            try {
+                manageMovies();
+            } catch(Exception e){
+                System.out.println("Exception " + e);
+            }
+            System.out.println("Test number " + i + " ended.");
+            System.out.println("-----------------------------------------------");
+        }
+        System.out.println("All tests have ended.");
     }
 }

fd0af0352eefc64e8758fdfff0ff939e3192ee26
itai solomon <52383917+ItaiSolo@users.noreply.github.com>
2026-06-06 14:20:40 +0300

    last fix 2

--- hw2/src/RentalSystem.java
+++ hw2/src/RentalSystem.java
@@ -10,7 +10,7 @@ public class RentalSystem {
     private int lastMovieIndex;
     private int lastDirectorIndex;
 
-    public RentalSystem(){
+    public RentalSystem() {
         this.movies = new Movie[MAX];
         this.directors = new Director[MAX];
         this.directorMovies = new int[MAX];
@@ -22,49 +22,46 @@ public class RentalSystem {
     }
 
     public void addMovie(String title, Genre genre, int releaseYear,
-                         String director, String biography){
-        if(lastMovieIndex == 30){
+                         String director, String biography) {
+        if (lastMovieIndex == 30) {
             System.out.println("System is full, Cannot add more movies");
-        }
-        else if(findMovie(title,releaseYear,director) != -1) {
+        } else if (findMovie(title, releaseYear, director) != -1) {
             System.out.println("Movie is already in the system");
-        }
-        else{
+        } else {
             int index = findDirector(director);
-            if(index == -1) {
+            if (index == -1) {
 
-                if (directors[0] == null && movies[0] == null){
-                    directors[lastDirectorIndex] = new Director(director,biography);
-                    movies[lastMovieIndex] = new Movie(title, releaseYear, directors[lastDirectorIndex],genre);
+                //if (directors[0] == null && movies[0] == null){
+                //  directors[lastDirectorIndex] = new Director(director,biography);
+                //movies[lastMovieIndex] = new Movie(title, releaseYear, directors[lastDirectorIndex],genre);
 
-                } else {
-                    lastDirectorIndex++;
-                    lastMovieIndex++;
-                    directors[lastDirectorIndex] = new Director(director,biography);
-                    movies[lastMovieIndex] = new Movie(title, releaseYear, directors[lastDirectorIndex],genre);
-                }
 
-            }
-            else {
+                directors[lastDirectorIndex] = new Director(director, biography);
+                movies[lastMovieIndex] = new Movie(title, releaseYear, directors[lastDirectorIndex], genre);
+                lastDirectorIndex++;
                 lastMovieIndex++;
-                movies[lastMovieIndex] = new Movie(title, releaseYear, directors[index],genre);
+
+
+            } else {
+
+                movies[lastMovieIndex] = new Movie(title, releaseYear, directors[index], genre);
+                lastMovieIndex++;
             }
         }
     }
 
-
-    public void removeMovie(String title, int releaseYear, String director){
+    public void removeMovie(String title, int releaseYear, String director) {
         int movieIndex = findMovie(title, releaseYear, director);
-        if (movieIndex == -1){
+        if (movieIndex == -1) {
             System.out.println("No such movie exists.");
             return;
         }
-        if (isRented(movieIndex)){
+        if (isRented(movieIndex)) {
             System.out.println("Cannot remove a rented movie.");
             return;
         }
         int directorIndex = findDirector(director);
-        if (directorMovies[directorIndex] == 1){
+        if (directorMovies[directorIndex] == 1) {
             directors[directorIndex] = null;
             directorMovies[directorIndex] = 0;
 
@@ -72,7 +69,7 @@ public class RentalSystem {
             directors[lastDirectorIndex] = null;
             directorMovies[directorIndex] = directorMovies[lastDirectorIndex];
             directorMovies[lastDirectorIndex] = 0;
-        } else if (directorMovies[directorIndex] > 1){
+        } else if (directorMovies[directorIndex] > 1) {
             directorMovies[directorIndex]--;
         }
 
@@ -80,128 +77,132 @@ public class RentalSystem {
         movies[lastMovieIndex] = null;
     }
 
-    public void printMovies(){
-        int lastUnrentedMoviesIndex = 0;
-        int lastRantedMoviesIndex = 0;
-        Movie[] unrentedMovies =  new Movie[MAX];
-        Movie[] rentedMovies =  new Movie[MAX];
+    public void printMovies() {
+        boolean allRented = true;
+        boolean allUnRented = true;
+        for (int i = 0; i < rented.length; i++) {
+            if (rented[i] != 0) {
+                allUnRented = false;
+            }
+            if (rented[i] == 0) {
+                allRented = false;
+            }
+        }
 
-        for(int i = 0; i < lastMovieIndex; i++) {
-            for (int j = 0; j < lastCustomerIndex; j++) {
-                if(customers[j].isRenting(movies[i])){
-                    rentedMovies[lastRantedMoviesIndex] = movies[i];
-                    lastRantedMoviesIndex++;
-                    break;
-                }
-                unrentedMovies[lastUnrentedMoviesIndex] = movies[i];
-                lastUnrentedMoviesIndex++;
-            }
-        }
-        if(lastUnrentedMoviesIndex == 0 && lastRantedMoviesIndex == 0){
-            System.out.println("No Unrented movies");
+        if (customers.length == 0 || allUnRented == true) {
             System.out.println("No Rented movies");
-            return;
-        }
-        if(lastRantedMoviesIndex == 0){
-            System.out.println("No Rented movies");
-        }else {
-            System.out.println("Rented movies: ");
-            for(int i = 0; i < lastRantedMoviesIndex; i++){
-                System.out.println(rentedMovies[i]);
-            }
-        }
-        if(lastUnrentedMoviesIndex == 0){
-            System.out.println("No Unrented movies");
-        }else {
             System.out.println("Unrented movies: ");
-            for (int i = 0; i < lastUnrentedMoviesIndex; i++) {
-                System.out.println(unrentedMovies[i]);
+            for (int i = 0; i < movies.length; i++) {
+                System.out.println(movies[i].toString());
+            }
+        } else if (movies.length == 0) {
+            System.out.println("No Unrented movies");
+            System.out.println("No Rented movies");
+            return;
+        } else if (allRented == true) {
+            System.out.println("No Unrented movies");
+            System.out.println("Rented movies: ");
+            for (int i = 0; i < movies.length; i++) {
+                System.out.println(movies[i]);
+            }
+        } else {
+            System.out.println("Rented movies: ");
+            for (int i = 0; i < movies.length; i++) {
+                if (rented[i] != 0) {
+                    System.out.println(movies[i]);
+                }
+            }
+            System.out.println("Unrented movies: ");
+            for (int i = 0; i < movies.length; i++) {
+                if (rented[i] == 0) {
+                    System.out.println(movies[i]);
+                }
             }
         }
     }
 
-    public void rentMovie(String name, String id, String title, int releaseYear, String director){
-        int customerIndex = findCustomer(id);
-        if (lastCustomerIndex >= MAX && customerIndex == -1){
-            System.out.println("No room for new customers.");
-            return;
-        }
-        int movieIndex = findMovie(title, releaseYear, director);
-        if (movieIndex == -1){
-            System.out.println("No such movie exists.");
-            return;
-        }
-
-        if (customerIndex != -1 && customers[customerIndex].isRenting(movies[movieIndex])){
-            System.out.println("Customer already has this movies.");
-            return;
-        }
-
-        if (customerIndex == -1) {
-            Customer customer = new Customer(name, id);
-            if (customers[0] == null){
-                customers[lastCustomerIndex] = customer;
-            } else {
-                customers[++lastCustomerIndex] = customer;
-            }
-            customerIndex = lastCustomerIndex;
-        }
-        boolean result = customers[customerIndex].rentMovie(movies[movieIndex]);
-        if (!result){
-            System.out.println("Customer has reached the limit");
-        }
+public void rentMovie(String name, String id, String title, int releaseYear, String director) {
+    int customerIndex = findCustomer(id);
+    if (lastCustomerIndex >= MAX && customerIndex == -1) {
+        System.out.println("No room for new customers.");
+        return;
     }
-
-    public void returnMovie(String id, String title, int releaseYear, String director){
-        int customerIndex = findCustomer(id);
-        if (customerIndex == -1){
-            System.out.println("Customer not found.");
-            return;
-        }
-        int movieIndex = findMovie(title, releaseYear, director);
-        if (movieIndex == -1 || customers[customerIndex].returnMovie(movies[movieIndex])){
-            System.out.println("Customer cannot return the movie.");
-            return;
-        }
-
-        rented[movieIndex]--;
-
-        if (customers[customerIndex].isEmpty()){
-            customers[customerIndex] = customers[lastCustomerIndex];
-            customers[lastCustomerIndex--] = null;
-        }
+    int movieIndex = findMovie(title, releaseYear, director);
+    if (movieIndex == -1) {
+        System.out.println("No such movie exists.");
         return;
     }
 
-    public int findMovie(String title, int releaseYear, String director){
-        for (int i = 0; i < lastMovieIndex; i++){
-            if (movies[i].equals(title, releaseYear, director)){
-                return i;
-            }
+    if (customerIndex != -1 && customers[customerIndex].isRenting(movies[movieIndex])) {
+        System.out.println("Customer already has this movies.");
+        return;
+    }
+
+    if (customerIndex == -1) {
+        Customer customer = new Customer(name, id);
+        if (customers[0] == null) {
+            customers[lastCustomerIndex] = customer;
+        } else {
+            customers[++lastCustomerIndex] = customer;
         }
-        return -1;
+        customerIndex = lastCustomerIndex;
+    }
+    boolean result = customers[customerIndex].rentMovie(movies[movieIndex]);
+    if (!result) {
+        System.out.println("Customer has reached the limit");
+    }
+}
+
+public void returnMovie(String id, String title, int releaseYear, String director) {
+    int customerIndex = findCustomer(id);
+    if (customerIndex == -1) {
+        System.out.println("Customer not found.");
+        return;
+    }
+    int movieIndex = findMovie(title, releaseYear, director);
+    if (movieIndex == -1 || customers[customerIndex].returnMovie(movies[movieIndex])) {
+        System.out.println("Customer cannot return the movie.");
+        return;
     }
 
-    public int findDirector(String directorName){
-        for(int i = 0; i < lastDirectorIndex; i++){
-            if(directors[i].equals(directorName)) {
-                return i;
-            }
+    rented[movieIndex]--;
+
+    if (customers[customerIndex].isEmpty()) {
+        customers[customerIndex] = customers[lastCustomerIndex];
+        customers[lastCustomerIndex--] = null;
+    }
+    return;
+}
+
+public int findMovie(String title, int releaseYear, String director) {
+    for (int i = 0; i < lastMovieIndex; i++) {
+        if (movies[i].equals(title, releaseYear, director)) {
+            return i;
         }
-        return -1;
     }
+    return -1;
+}
 
-    public int findCustomer(String id){
-        for (int i = 0; i < lastCustomerIndex; i++){
-            if (customers[i] != null && customers[i].equals(id)){
-                return i;
-            }
+public int findDirector(String directorName) {
+    for (int i = 0; i < lastDirectorIndex; i++) {
+        if (directors[i].equals(directorName)) {
+            return i;
         }
-        return -1;
     }
+    return -1;
+}
+
+public int findCustomer(String id) {
+    for (int i = 0; i < lastCustomerIndex; i++) {
+        if (customers[i].equals(id)) {
+            return i;
+        }
+    }
+    return -1;
+}
 
 
-    public boolean isRented(int index){
-        return this.rented[index] > 0;
-    }
+public boolean isRented(int index) {
+    return this.rented[index] > 0;
+}
 }

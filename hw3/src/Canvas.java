6a924e938ed53dc6999bc2898e94dd3ca465caa6
itai solomon <52383917+ItaiSolo@users.noreply.github.com>
2026-06-20 11:27:41 +0300

    hw3 start

--- hw3/src/Canvas.java
+++ hw3/src/Canvas.java
@@ -1,2 +1,13 @@
 public class Canvas {
+    public String getTotalPerimeter() {
+    }
+
+    public String getTotalArea() {
+    }
+
+    public void addShape(Circle circle, int i, int i1) {
+    }
+
+    public void removeShape(int i, int i1) {
+    }
 }

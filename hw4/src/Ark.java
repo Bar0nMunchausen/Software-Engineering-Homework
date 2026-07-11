74ae1e85fe87806a11dee758e6492deae6f874ff
Mucnhausen <74404092+Mucnhausen@users.noreply.github.com>
2026-07-11 14:29:32 +0300

    Add javadoc to Ark class. Remove redundant .toString

--- hw4/src/Ark.java
+++ hw4/src/Ark.java
@@ -2,28 +2,48 @@ public class Ark {
 
     private SpeciesQueue<Animal> queue;
 
+    /**
+     * Initialize an empty Ark object.
+    **/
     public Ark(){
         this.queue = new SpeciesQueue<Animal>();
     }
 
+    /**
+     * Removes and prints the first animal in the queue.
+     */
     public void enterToArk() {
         System.out.println("A " + this.queue.remove() + " entered the ark");
     }
 
+    /**
+     * Removes and prints ALL the animals in the queue.
+     */
     public void enterAllToArk() {
         while(!this.queue.isEmpty()){
             enterToArk();
         }
     }
 
+    /**
+     * Prints ALL the animals in the queue.
+     */
     public void showQueue() {
-         System.out.println(this.toString());
+         System.out.println(this);
     }
 
+    /**
+     * Adds the given animal to the queue.
+     * @param animal The animal to add.
+     */
     public void add(Animal animal) {
         this.queue.add(animal);
     }
 
+    /**
+     * Builds and returns the list of all animals in the queue
+     * @return List of all animals.
+     */
     @Override
     public String toString(){
         if (this.queue == null) return "";

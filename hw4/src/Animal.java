fb602ead0e0efa2e4c212414ef65633293566a46
Mucnhausen <74404092+Mucnhausen@users.noreply.github.com>
2026-07-11 14:33:32 +0300

    Make Animal abstract

--- hw4/src/Animal.java
+++ hw4/src/Animal.java
@@ -1,4 +1,4 @@
-public class Animal implements Comparable<Animal>, Cloneable {
+public abstract class Animal implements Comparable<Animal>, Cloneable {
     /**
      * Abstract base class representing an animal
      */
@@ -12,12 +12,12 @@ public class Animal implements Comparable<Animal>, Cloneable {
         this.dominance = dominance;
     }
 
-    @Override
     /**
      * Compares this animal with another based on dominance.
      * @param other The animal to compare against.
      * @return Positive if more dominant, negative if less, 0 if equal
      */
+    @Override
     public int compareTo(Animal other){
         return this.dominance - other.dominance;
     }
@@ -27,8 +27,7 @@ public class Animal implements Comparable<Animal>, Cloneable {
      * @return A copied Animal instance, or null if cloning fails.
      */
     public Animal clone() throws CloneNotSupportedException {
-        Animal animal = (Animal) super.clone();
-        return animal;
+        return (Animal) super.clone();
     }
 
     /**
